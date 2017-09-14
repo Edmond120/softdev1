53765aa6cc545ea9f541fb8487be8ac6e514fc1e
edmond120 <edmondwong888@msn.com>
2017-09-14 14:20:40 -0400

    added work

---
+++ rememberPy.py
@@ -0,0 +1,22 @@
+import random
+import sys
+#sys.argv[1]
+CLASSES = {
+    7: [ 'Helen', 'Shakil', 'Eric', 'Jennifer Y', 'Jennifer Z', 'Arif', 'Queenie', 'Jawadul', 'Shaina', 'Vivien', 'Brian', 'Naotaka', 'Bayan', 'Adam', 'Caleb', 'Terry', 'Jason', 'Alessandro', 'Samantha', 'Carol', 'Joyce', 'Shannon', 'Charles', 'Taylor', 'Kelly', 'Leo', 'Khyber', 'Ibnul', 'Eugene', 'Yuyang', 'Karina', 'Tiffany', 'Holden', 'Michael'],
+    8: ['Masha', 'Adrian', 'David', 'Eric', 'Josh', 'Jerome', 'Henry', 'Jackie', 'Giorgio', 'Karen', 'Sonal', 'Xavier', 'Bermet', 'Alex', 'Iris', 'Manahal', 'Donia', 'Md', 'Connie', 'Lisa', 'Xing', 'Angelica', 'Angel', 'Augie', 'Dimitriy', 'Yiduo', 'Gordon', 'Tiffany', 'Clive', 'Jonathan', 'Sasha', 'Daniel'],
+    9: [ 'Yu Qi', 'Michela', 'Kristin', 'Fabiha', 'Maxim', 'Marcus', 'Ish', 'James', 'Ryan', 'Edward', 'Adeeb', 'Jake', 'Cynthia', 'Kevin', 'Levi', 'Edmond', 'Kyle', 'Andrew', 'Max', 'Jenny', 'Philip', 'Shan', 'Mansour', 'Ray', 'Jake', 'Ida', 'Kerry', 'Stanley', 'Jackie', 'William', 'Tina', 'Michael']
+}
+
+randPd = int( random.random() * 3 + 7)
+#print len(sys.argv)
+if(len(sys.argv) >= 3 and sys.argv[1] == 'search'):
+    if(sys.argv[2] in CLASSES[7]):
+        print 7
+    elif(sys.argv[2] in CLASSES[8]):
+        print 8
+    elif(sys.argv[2] in CLASSES[9]):
+        print 9
+    else:
+        print 'not found'
+else:
+    print CLASSES[randPd][int(random.random() * len(CLASSES[randPd]))]
